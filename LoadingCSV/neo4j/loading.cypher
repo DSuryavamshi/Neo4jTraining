248b64f99aadd59a72708833d57db72bafb896cf
Dhanush Suryavamshi <suryavamshi221b@gmail.com>
2021-11-06 21:20:48 +0530

    importing CSV

---
+++ LoadingCSV/neo4j/loading.cypher
@@ -0,0 +1,13 @@
+// CSV files must be stored in import folder of the project.
+LOAD CSV WITH HEADERS FROM 'file:///users.csv' AS row
+MERGE (u:User {name:row.Name, fav_genre:row.Fav_genre, preferred_language:row.Preferred_language})
+MERGE (m:Movies {title: row.Seen})
+MERGE (u)-[:seen {liked:row.Liked, rated:row.Rated}]->(m)
+
+
+LOAD CSV WITH HEADERS FROM 'file:///movies.csv' AS row
+MATCH (m:Movies {title:row.title})
+SET m.rating = row.rating 
+SET m.released = row.released 
+SET m.genre = row.genre 
+SET m.language = row.language;
---
+++ LoadingCSV/python/load_csv.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+from GettingStarted.python.Neo4jConnection import Neo4jConnection
+
+
+if __name__ == "__main__":
+    conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="moviedb")
+
+    users = f"""
+    LOAD CSV WITH HEADERS FROM 'file:///users.csv' AS row
+    MERGE (u:User {{name:row.Name, fav_genre:row.Fav_genre, preferred_language:row.Preferred_language}})
+    MERGE (m:Movies {{title: row.Seen}})
+    MERGE (u)-[:seen {{liked:row.Liked, rated:row.Rated}}]->(m)
+    """
+    users_load = conn.query(query=users, db="moviescsv")
+    print(users_load)
+    movie = f"""
+    LOAD CSV WITH HEADERS FROM 'file:///movies.csv' AS row
+    MATCH (m:Movies {{title:row.title}})
+    SET m.rating = row.rating 
+    SET m.released = row.released 
+    SET m.genre = row.genre 
+    SET m.language = row.language
+    """
+    movies_set = conn.query(query=movie, db="moviescsv")
+    print(movies_set)
